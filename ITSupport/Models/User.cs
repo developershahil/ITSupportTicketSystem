e858592eb951ce1146e10908480d1397585b2c08
Rathod sahil <sahilrathod222@gmail.com>
2026-02-04 16:11:45 +0530

    Replace plaintext Password with PasswordHash in User model

--- ITSupport/Models/User.cs
+++ ITSupport/Models/User.cs
@@ -8,16 +8,21 @@ namespace ITSupport.Models
         public int UserId { get; set; }
 
         [Required]
-        public string FullName { get; set; }
-
-        [Required, EmailAddress]
-        public string Email { get; set; }
+        [StringLength(100)]
+        public string FullName { get; set; } = string.Empty;
 
         [Required]
-        public string Password { get; set; } // hashed later
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        // Store hashed password only
+        [StringLength(512)]
+        public string? PasswordHash { get; set; }
 
         [Required]
-        public string Role { get; set; } // Admin / Support / Technician
+        [StringLength(50)]
+        public string Role { get; set; } = "Support";
 
         public bool IsActive { get; set; } = true;
     }
